953a3a7aa92ebc73c54f86e4ff6d720f5f1694c0
yusfi <yadilaksa@gmail.com>
2019-11-07 11:16:55 +0700

    edit signup

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -115,7 +115,7 @@
                                 <div class="form-group row">
                                     <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
                                     <div class="col-md-6">
-                                        <input type="password" id="password" class="form-control" name="password" required>
+                                        <input type="password~" id="password" class="form-control" name="password" required>
                                     </div>
                                 </div>
                                 <div class="col-md-6 offset-md-4">
